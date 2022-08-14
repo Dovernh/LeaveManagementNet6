a5c0acebfb34a8480eef692eb640f58311c2d50c
Paul H <dovernh@gmail.com>
2022-08-14 16:48:45 -0400

    leave types

---
+++ LeaveManagementWeb/Models/LeaveTypeVM.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementWeb.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
